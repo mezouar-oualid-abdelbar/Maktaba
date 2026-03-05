7fdbdf589c418f81b78968eb892f410fc7334560
vcx-0047 <mezouarwalid.0047@gmail.com>
2026-03-05 15:48:47 +0100

    tp ex1->3

--- app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
+++ app/src/main/java/com/ElOuedUniv/maktaba/data/repository/BookRepository.kt
@@ -23,11 +23,17 @@ class BookRepository {
      * )
      */
     private val booksList = listOf(
-        Book(isbn = "", title = "Clean Code", nbPages = 0),
-        Book(isbn = "", title = "The Pragmatic Programmer", nbPages = 0),
-        Book(isbn = "", title = "Design Patterns", nbPages = 0),
-        Book(isbn = "", title = "Refactoring", nbPages = 0),
-        Book(isbn = "", title = "Head First Design Patterns", nbPages = 0)
+        Book(isbn = "9780135398548", title = "Clean Code", nbPages =672),
+        Book(isbn = "9780132119177", title = "The Pragmatic Programmer", nbPages = 352),
+        Book(isbn = "9780201715941", title = "Design Patterns", nbPages = 334),
+        Book(isbn = "9780201485677", title = "Refactoring", nbPages = 431),
+        Book(isbn = "9780596007126", title = "Head First Design Patterns", nbPages = 638),
+        Book(isbn = "9781093123654", title = "Laravel Doc", nbPages = 771) ,
+        Book(isbn = "9780131103627", title = "The C programming language", nbPages =272 ),
+        Book(isbn = "9780470087251", title = "Professional Web APIs with PHP", nbPages = 356),
+        Book(isbn = "9780071605502", title = "MySQL Database Usage & Administration", nbPages =500 ),
+        Book(isbn = "9780596555139", title = "Linux System Programming", nbPages =392 )
+
     )
 
     /**
@@ -47,6 +53,7 @@ class BookRepository {
      * @return List of all books
      */
     fun getAllBooks(): List<Book> {
+//        add api call
         return booksList
     }
 
